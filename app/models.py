ec9d5e27f62db83c9c50e8a8d0ff1c9183b69780
James McMillan <jmcmillan29@gmail.com>
2019-10-23 20:50:31 -0700

    initial add of werkzeug security module and initializing it

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
 
@@ -19,3 +20,9 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
