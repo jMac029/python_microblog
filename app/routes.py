320f13df30ae1861565f6f44c831b1313eaf1088
James McMillan <jmcmillan@onica.com>
2019-10-24 11:24:58 -0700

    added in the route for the user profile pages

--- app/routes.py
+++ app/routes.py
@@ -24,6 +24,16 @@ def index():
     ]
     return render_template('index.html', title='Flask', posts=posts)
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
