154a56d9e50e791f703beb9bb11722853ba06352
James McMillan <jmcmillan@onica.com>
2019-10-28 14:55:32 -0700

    updated the site to use the bootstrap css framework for a better ui/ux experience

--- app/__init__.py
+++ app/__init__.py
@@ -12,12 +12,12 @@ import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
-bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
